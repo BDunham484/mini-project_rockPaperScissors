7bdbaf3e768b4199ee0cb114edf2d5667d1ee17a
ThereWolf <bdunham484@gmail.com>
2023-03-16 15:45:40 -0500

    update game

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
-let rps = ['r', 'p', 's'];
-// let response;
 let compResponse;
 let playerWins = 0;
 let compWins = 0;
+let ties = 0;
 
-let rpsFunc = () => {
-    let num = Math.floor(Math.random() * ((3 - 1) + 1)) + 1
-
-    if (num === 1) {
-        let compResponse = "R";
-        return compResponse;
-    } else if (num === 2) {
-        let compResponse = "P";
-        return compResponse;
-    } else {
-        let compResponse = "S";
-        return compResponse;
+let playGame = () => {
+    let rpsFunc = () => {
+        let num = Math.floor(Math.random() * ((3 - 1) + 1)) + 1
+    
+        if (num === 1) {
+            let compResponse = "R";
+            return compResponse;
+        } else if (num === 2) {
+            let compResponse = "P";
+            return compResponse;
+        } else {
+            let compResponse = "S";
+            return compResponse;
+        }
     }
-}
+    
+    compResponse = rpsFunc();
 
-compResponse = rpsFunc();
-
-let playGame = (compResponse) => {
-    let response = window.prompt('Please enter "R", "P", or "S".');
+    let response = window.prompt('Please enter "R", "P", or "S".').toUpperCase();
 
     if (response === 'R' && compResponse === 'R') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
+        ties++;
         console.log('Tie');
     } else if (response === 'R' && compResponse === 'P') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         compWins++;
         console.log('Computer Wins');
     } else if (response === 'R' && compResponse === 'S') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         playerWins++;
         console.log("Player Wins");
     } else if (response === 'P' && compResponse === 'P') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
+        ties++;
         console.log('Tie');
     } else if (response === 'P' && compResponse === 'R') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         playerWins++;
         console.log("Player Wins");
     } else if (response === 'P' && compResponse === 'S') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         compWins++;
         console.log('Computer Wins');
     } else if (response === 'S' && compResponse === 'S') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
+        ties++;
         console.log('Tie');
     } else if (response === 'S' && compResponse === 'P') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         playerWins++;
         console.log("Player Wins");
     } else if (response === 'S' && compResponse === 'R') {
         console.log("Player: " + response + '\n' +  "Computer: " + compResponse)
-
         compWins++;
         console.log('Computer Wins');
     } else {
         console.log("I don't know what just happened.");
-    }
+    };
 
-    
-}
+    window.alert("Totals: \n Player Wins: " + playerWins + " \n Computer Wins: " + compWins + "\n Ties: " + ties);
+
+    if (window.confirm('Would you like to play Rock, Paper, Scissors?')) {
+        playGame();
+    };
+};
 
 if (window.confirm('Would you like to play Rock, Paper, Scissors?')) {
-    playGame(compResponse);
+    playGame();
 }
 
 
